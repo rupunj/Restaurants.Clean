b20fd5c82c59d0431170d3521c52a147f69c88a9
Rupun Chathuranga <rupun.cj@gmail.com>
2024-05-31 15:54:47 +0530

    Fluent Validation apply Create Restaurant

--- Restaurants.Clean.Application/Extentions/ServiceCollection.cs
+++ Restaurants.Clean.Application/Extentions/ServiceCollection.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-
+using FluentValidation;
+using FluentValidation.AspNetCore;
 namespace Restaurants.Clean.Application;
 
 public static class ServiceCollection
 {
     public static void ApplicationService(this IServiceCollection services)
     {
+        var AssemblyService = typeof(RestaurantsService).Assembly;
         services.AddScoped<IRestaurantService,RestaurantsService>();
-        services.AddAutoMapper(typeof(RestaurantsService).Assembly);
+        services.AddAutoMapper(AssemblyService);
+        services.AddValidatorsFromAssembly(AssemblyService).AddFluentValidationAutoValidation();
 
     }
 
---
+++ Restaurants.Clean.Application/Restaurants/Validators/CreateRestaurantDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using FluentValidation;
+namespace Restaurants.Clean.Application;
+
+public class CreateRestaurantDtoValidator :AbstractValidator<CreateResturantDto>
+{
+    public CreateRestaurantDtoValidator()
+    {
+        RuleFor(dto=> dto.Name)
+        .Length(3,100);
+
+        RuleFor(dot=> dot.Discription)
+        .NotEmpty().WithMessage("Discription is required");
+
+        
+        
+        
+    }
+
+}
