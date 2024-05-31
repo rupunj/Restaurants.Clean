21150e4b90e4e5fff4833045105fb520224e2adc
Rupun Chathuranga <rupun.cj@gmail.com>
2024-05-31 09:51:26 +0530

    adding controller

---
+++ Restaurants.Clean.API/Controllers/RestaurantsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.Clean.API;
+[ApiController]
+[Route("api/Restaurants")]
+public class RestaurantsController:ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+
+}
