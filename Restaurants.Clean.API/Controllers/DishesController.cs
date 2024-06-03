b2e716b4375b5be68a00c24bce9e0805504c2165
Rupun Chathuranga <rupun.cj@gmail.com>
2024-06-03 15:59:24 +0530

    Handling Subentities

---
+++ Restaurants.Clean.API/Controllers/DishesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace Restaurants.Clean.API;
+
+[ApiController]
+[Route("api/Restaurans/{restaurantId}/Dishes")]
+public class DishesController :ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetDishes([FromRoute] int restaurantId )
+    {
+        return Ok();
+    }
+
+}
