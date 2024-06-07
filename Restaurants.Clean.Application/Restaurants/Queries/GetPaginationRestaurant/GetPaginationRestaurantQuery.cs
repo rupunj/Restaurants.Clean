7236ddd7ea6c370262e60b9bbacb6a9ff851e796
Rupun Chathuranga <rupun.cj@gmail.com>
2024-06-07 14:31:01 +0530

    Validator for pagineation query

--- Restaurants.Clean.Application/Restaurants/Queries/GetPaginationRestaurant/GetPaginationRestaurantQuery.cs
+++ Restaurants.Clean.Application/Restaurants/Queries/GetPaginationRestaurant/GetPaginationRestaurantQuery.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using System.Runtime.CompilerServices;
+using FluentValidation;
+using MediatR;
 namespace Restaurants.Clean.Application;
 
 public class GetPaginationRestaurantQuery:IRequest<PageResult<RestaurantsDto>>
@@ -6,4 +8,17 @@ public class GetPaginationRestaurantQuery:IRequest<PageResult<RestaurantsDto>>
     public string? SearchQuery { get; set; }
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
+}
+public class GetPaginationRestaurantQueryValidator : AbstractValidator<GetPaginationRestaurantQuery>
+{
+    private int[] AllowPageSize = [5,10,15,30];
+    public GetPaginationRestaurantQueryValidator()
+    {
+        RuleFor(q=>q.PageNumber)
+        .GreaterThanOrEqualTo(1);
+
+        RuleFor(q=>q.PageSize)
+        .Must(value=> AllowPageSize.Contains(value))
+        .WithMessage($"Page Size must be in [{string.Join(",",AllowPageSize)}]");
+    }
 }
