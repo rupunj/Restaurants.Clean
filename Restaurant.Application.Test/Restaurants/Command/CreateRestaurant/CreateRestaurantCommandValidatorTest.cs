58d657d85af061d6e1879f893b5dd2006c811e9d
Rupun Chathuranga <rupun.cj@gmail.com>
2024-06-12 11:33:36 +0530

    Validator Command

---
+++ Restaurant.Application.Test/Restaurants/Command/CreateRestaurant/CreateRestaurantCommandValidatorTest.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using Restaurants.Clean.Application;
+
+namespace Restaurant.Application.Test;
+
+public class CreateRestaurantCommandValidatorTest
+{
+    [Fact]
+    public void Validator_ForValidCommand_ShouldNotHaveValidationErrors()
+    {
+        // arrange
+
+        var command = new CreateRestaurantCommand()
+        {
+            Name = "Test",
+            Discription = "Test",
+            ContactEmail = "test@test.com",
+            Category = "Italian"
+        };
+    
+        // act
+        var validator = new CreateRestaurantCommandValidator();
+        var result = validator.TestValidate(command);
+
+        // assert
+        result.IsValid.Should().BeTrue();
+    }
+    [Fact]
+    public void Validator_ForInvalidCommand_ShouldHaveValidationErrors()
+    {
+        // arrange
+        var command = new CreateRestaurantCommand()
+        {
+            Name = "Test",
+            Discription = "Test",
+            ContactEmail = "test@test",
+            Category = "SriLankan"
+        };
+        // act
+        var validator = new CreateRestaurantCommandValidator();
+        var result = validator.TestValidate(command);
+
+        //assert
+       
+        result.IsValid.Should().BeFalse();
+        
+    }
+    [Theory]
+    [InlineData("Chinese")]
+    [InlineData("Italian")]
+    public void Validator_ForValidCategory_ShouldNotHaveValidationErrors(string category)
+    {
+        // arrange
+        var command = new CreateRestaurantCommand()
+        {
+            Category = category
+        };
+        // act
+        var validator = new CreateRestaurantCommandValidator();
+        var result = validator.TestValidate(command);
+        // assert
+
+        result.ShouldNotHaveValidationErrorFor(q=> q.Category);
+
+    }
+}
--- Restaurants.Clean.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandValidator.cs
+++ Restaurants.Clean.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandValidator.cs
@@ -3,6 +3,7 @@ namespace Restaurants.Clean.Application;
 
 public class CreateRestaurantCommandValidator :AbstractValidator<CreateRestaurantCommand>
 {
+    public readonly List<string> ValidCategories=["Italian", "Spanish", "Chinese"];
     public CreateRestaurantCommandValidator()
     {
         RuleFor(dto=> dto.Name)
@@ -10,6 +11,10 @@ public class CreateRestaurantCommandValidator :AbstractValidator<CreateRestauran
 
         RuleFor(dot=> dot.Discription)
         .NotEmpty().WithMessage("Discription is required");
+
+        RuleFor(dto=> dto.Category)
+       .Must(q=> ValidCategories.Contains(q))
+       .WithMessage($"Category must be one of {string.Join(",",ValidCategories)}");
     }
 
 }
